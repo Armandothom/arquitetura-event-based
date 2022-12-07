ebbcdccb36ffe33eade121a6e90ed61fa16d264a
Jair Brann Junior <jairjunior02@hotmail.com>
2022-12-07 18:26:54 -0300

    Front

---
+++ front/src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { webSocket } from "rxjs/webSocket";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  email = new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$')]);
+  password = new FormControl('', Validators.required);
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+  login() {
+    const subject = webSocket('wss://localhost:3000');
+    const email = this.email.value as string;
+    const password = this.password.value as string;
+
+    subject.subscribe(
+      msg => console.log('message received: ' + msg),
+      err => console.log(err),
+      () => console.log('complete')
+    );
+
+    subject.next({message: 'login', payload: {email: '', password: ''}});
+    subject.complete();
+  }
+
+}
